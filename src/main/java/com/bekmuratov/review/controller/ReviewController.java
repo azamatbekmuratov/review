93434e6783c233b7ef78c82e4a03fadb8a86f848
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-09-15 15:59:58 +0600

    feat: added rest controller

---
+++ src/main/java/com/bekmuratov/review/controller/ReviewController.java
@@ -0,0 +1,28 @@
+package com.bekmuratov.review.controller;
+
+import com.bekmuratov.review.domain.dto.ProductReviewDto;
+import com.bekmuratov.review.service.api.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Azamat Bekmuratov
+ */
+@RestController
+@RequestMapping
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<?> findReviewByProductId(@PathVariable String productId){
+        ProductReviewDto productReviewDto = reviewService.findReviewByProductId(productId);
+        return ResponseEntity.ok(productReviewDto);
+    }
+
+}
